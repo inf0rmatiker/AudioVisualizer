e563666c4f8b0ad7f0c617fee471cd8a30bdcaa4
Caleb Carlson <ccarlson355@gmail.com>
2019-01-07 15:19:15 -0700

    Created a functioning wave visualizer

--- vuMeter.py
+++ vuMeter.py
@@ -9,12 +9,34 @@ import time
 from scipy.fftpack import fft
 import os
 import struct
+import sys
 from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 
 class AudioVisualizer(object):
 
 	def __init__(self, device_index, pyaudio_object):
+
+		# pyqtgraph stuff
+		pg.setConfigOptions(antialias=True)
+		self.traces = dict()
+		self.app = QtGui.QApplication(sys.argv)
+		self.win = pg.GraphicsWindow(title='Audio Visualizer')
+		self.win.setWindowTitle('Audio Visualizer')
+		self.win.setGeometry(5, 115, 1910, 1070)
+
+		wf_xlabels = [(0, '0'), (2048, '2048'), (4096, '4096')]
+		wf_xaxis = pg.AxisItem(orientation='bottom')
+		wf_xaxis.setTicks([wf_xlabels])
+
+		wf_ylabels = [(-1, '-1'), (0, '0'), (1, '1')]
+		wf_yaxis = pg.AxisItem(orientation='left')
+		wf_yaxis.setTicks([wf_ylabels])
+
+
+		self.waveform = self.win.addPlot(
+			title='WAVEFORM', row=1, col=1, axisItems={'bottom': wf_xaxis, 'left': wf_yaxis},
+		)
 
 		# constants
 		self.RATE = 44100
@@ -34,30 +56,32 @@ class AudioVisualizer(object):
                     input_device_index = self.deviceIndex,
                     as_loopback = self.useloopback)
 
-		self.startLoopbackStreaming("wave")
+		self.x = np.arange(0, 2 * self.CHUNK, 2)
 
+	def start(self):
+		if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+			QtGui.QApplication.instance().exec_()
+
+	def set_plotdata(self, name, data_x, data_y):
+		if name in self.traces:
+			self.traces[name].setData(data_x, data_y)
+		else:
+			if name == 'waveform':
+				self.traces[name] = self.waveform.plot(pen='b', width=3)
+				self.waveform.setYRange(-1.5, 1.5, padding=0)
+				self.waveform.setXRange(0, 2 * self.CHUNK, padding=0.005)
 
 	def startLoopbackStreaming(self, graphType="wave"):
 		data = np.frombuffer(self.stream.read(self.CHUNK), dtype=self.DATA_TYPE)
 		data = [element/2**15. for element in data[:len(data)//2]] # normalize between -1 and 1 and cut list in half
 
-		# Plot the raw data
-		plt.plot(data)
-		plt.show()
-		plt.pause(5)
-	
-		plt.close()
-		self.stream.stop_stream()
-		self.stream.close()
-		self.p.terminate()
+		self.set_plotdata(name='waveform', data_x=self.x, data_y=data,)
 
-
-
-def main():
-	device_index, p = chooseDevice()
-	av = AudioVisualizer(device_index, p)
-
-	
+	def animation(self):
+		timer = QtCore.QTimer()
+		timer.timeout.connect(self.startLoopbackStreaming)
+		timer.start(20)
+		self.start()
 
 
 def chooseDevice():
@@ -65,7 +89,6 @@ def chooseDevice():
 
 	p = pyaudio.PyAudio()
 	device_info = {} 
-	useloopback = True
 
 	#Set default to first in list or ask Windows
 	try:
@@ -94,6 +117,12 @@ def chooseDevice():
 
 	print("Chosen device index: " + str(device_id))
 	return device_id, p
+
+
+def main():
+	device_index, p = chooseDevice()
+	av = AudioVisualizer(device_index, p)
+	av.animation()
 
 
 if __name__ == '__main__':
