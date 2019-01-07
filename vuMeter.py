5b707433fbf890770bf212191615bd20825f80b3
Caleb Carlson <ccarlson355@gmail.com>
2019-01-06 18:15:08 -0700

    Added list of available devices

--- vuMeter.py
+++ vuMeter.py
@@ -1,9 +1,43 @@
 # Author: Caleb Carlson
 # Date Created: 1/6/2019
 
+import pyaudio
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from scipy.fftpack import fft
+import os
+import struct
+
+RATE = 44100
+CHUNK = 1024
 
 def main():
 	print("Audio Visualizer (under development)...")
+
+	device_info = {} 
+	useloopback = False
+
+	p = pyaudio.PyAudio()
+
+	#Set default to first in list or ask Windows
+	try:
+		default_device_index = p.get_default_input_device_info()
+	except IOError:
+		default_device_index = -1
+
+	#Select Device
+	print ("Available devices:\n\n")
+	for i in range(0, p.get_device_count()):
+		info = p.get_device_info_by_index(i)
+		print (str(info["index"]) + ": \t %s \n \t %s \n" % (info["name"], p.get_host_api_info_by_index(info["hostApi"])["name"]))
+
+		if default_device_index == -1:
+			default_device_index = info["index"]
+
+
+
+
 
 
 if __name__ == '__main__':
